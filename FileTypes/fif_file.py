33c58840de96b2de549a3ca9a4152045c7523a11
monkeyman192 <monkey_man_192@yahoo.com.au>
2018-09-06 13:30:52 +1000

    added fif file template

---
+++ FileTypes/fif_file.py
@@ -0,0 +1,12 @@
+from .FileInfo import FileInfo
+
+
+class fif_file(FileInfo):
+    """
+    .fif specific file container.
+    """
+    def __init__(self, id_=None, file=None, *args, **kwargs):
+        super(fif_file, self).__init__(id_, file, *args, **kwargs)
+        self._type = '.fif'
+        self.display_raw = True
+        self.requires_save = False
