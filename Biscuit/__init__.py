b39658777101e451ce46f7c46f71c23f798a2815
monkeyman192 <monkey_man_192@yahoo.com.au>
2018-11-23 13:30:52 +1100

    slight fix for non-pip installed testing

--- Biscuit/__init__.py
+++ Biscuit/__init__.py
@@ -1,4 +1,2 @@
 __version__ = '1.0dev0'
 name = "Biscuit"  # noqa
-
-from .Windows.MainWindow import MainWindow  # noqa
--- Biscuit/__main__.py
+++ Biscuit/__main__.py
@@ -1,11 +1,14 @@
 def main():
     # Whatever your execution code is
     from tkinter import Tk
-    from .Windows import MainWindow
-
     import os
     from os.path import dirname
     os.chdir(dirname(__file__))
+
+    if __name__ == "__main__":
+        from Windows import MainWindow
+    else:
+        from .Windows import MainWindow
 
     root = Tk()
 
