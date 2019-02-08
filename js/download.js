209be1a3ad9f958f5cf8573ce9e0de25cd68e3ae
monkeyman192 <monkey_man_192@yahoo.com.au>
2019-02-08 14:16:44 +1100

    update site to have correct links to "complete" and "standard" install versions

--- js/download.js
+++ js/download.js
@@ -1,4 +1,4 @@
-function GetReleases()
+function GetReleases(idx)
 {
     $.getJSON("https://api.github.com/repos/Macquarie-MEG-Research/Biscuit/releases").done(function (json){
         // most recent release will be first entry in list
@@ -6,7 +6,7 @@ function GetReleases()
         if (release.assets.length === 0){
             alert('Something went wrong! Please raise an issue on GitHub!');
         }
-        var asset = release.assets[0];
+        var asset = release.assets[idx];
         window.location.href = asset.browser_download_url;
     });
 }
